08b77eb9d8fb3155368695618406d214680755d6
Stephen Jones <me@stephenj.co.uk>
2024-06-29 18:04:56 +0100

    Add raygun hit effect & shader

--- player/vorticon_guard.cs
+++ player/vorticon_guard.cs
@@ -12,18 +12,27 @@ public partial class vorticon_guard : CharacterBody2D
 	public const float JumpVelocity = -315.0f;
 	private bool isActivated = false;
 
+
 	private AnimatedSprite2D animation;
 	private CollisionShape2D collisionShape;
 
+	private Timer hitTimer;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		animation = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 		collisionShape = GetNode<CollisionShape2D>("CollisionShape2D");
+		hitTimer = GetNode<Timer>("HitTimer");
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+    private void HitTimeout()
+    {
+        (animation.Material as ShaderMaterial).SetShaderParameter("line_thickness", 0);
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
 	{
 		if (!isActivated)
 		{
@@ -56,6 +65,9 @@ public partial class vorticon_guard : CharacterBody2D
 
 	public void TakeDamage()
 	{
+		(animation.Material as ShaderMaterial).SetShaderParameter("line_thickness", 1);
+		hitTimer.Start();
+
 		Health--;
 		Debug.WriteLine("Vorticon Guard took damage! Health: " + Health);
 		if (Health <= 0)
