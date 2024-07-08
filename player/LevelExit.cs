ce951c93d0252e21b13f1072cc71ca49e1654318
Stephen Jones <me@stephenj.co.uk>
2024-07-08 22:54:27 +0100

    Add level exit

---
+++ player/LevelExit.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+using System.Diagnostics;
+
+public partial class LevelExit : Area2D
+{
+	private bool inRange = false;
+	private AudioStreamPlayer audioStreamPlayer;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		audioStreamPlayer = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
+		audioStreamPlayer.Finished += OnAudioStreamPlayerFinished;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (inRange && !audioStreamPlayer.Playing)
+		{
+			inRange = false;
+			audioStreamPlayer.Play();
+		}
+	}
+
+    private void OnAudioStreamPlayerFinished()
+    {
+        GetTree().ChangeSceneToFile("res://scenes/ck1-overworld.tscn");
+    }
+
+    public void OnBodyEntered(Node2D body)
+	{
+		Debug.Print("Level Exit reached");
+		if (body is keen)
+		{
+			inRange = true;
+		}
+	}
+}
