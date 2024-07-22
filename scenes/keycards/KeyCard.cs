a2081b0bfa93b808faaf93ad88f687afb2fe9c77
Stephen Jones <me@stephenj.co.uk>
2024-07-22 17:47:36 +0100

    Add Keycards

---
+++ scenes/keycards/KeyCard.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+using System.Diagnostics;
+
+public partial class KeyCard : Area2D
+{
+	[Export]
+    public game_stats.KeyCards Card = game_stats.KeyCards.Yellow;
+
+	private AudioStreamPlayer2D audioStreamPlayer;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		audioStreamPlayer = GetNode<AudioStreamPlayer2D>("AudioStreamPlayer2D");
+	}
+
+	public void OnBodyEntered(Node2D body)
+	{
+		if (body is keen keen)
+		{
+			this.SetDeferred("monitoring", false);
+			keen.GiveKey(Card);
+			audioStreamPlayer.Play();
+			
+			var tween = GetTree().CreateTween();
+			tween.Parallel().TweenProperty(this, "position", Position - new Vector2(0, 16), 1f);
+			tween.Parallel().TweenProperty(this, "modulate:a", 0, 1f);
+			tween.TweenCallback(Callable.From(QueueFree));
+		}
+	}
+}
