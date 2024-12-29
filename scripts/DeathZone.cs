f6daa3d9c31425f584fc496c862cc511e53b8e8e
Stephen Jones <me@stephenj.co.uk>
2024-12-29 14:31:21 +0000

    Create way to kill keen and show it

---
+++ scripts/DeathZone.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class DeathZone : Area2D
+{
+	private SignalManager signalManager;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		signalManager = GetNode<SignalManager>("/root/SignalManager");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void OnDeathZoneBodyEntered(Node2D body)
+	{
+		if (body is keen)
+		{
+			signalManager.EmitSignal(nameof(SignalManager.KeenDead));
+		}
+	}
+}
