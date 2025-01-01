4a8ec037451063cf709677aab8b0746d2ad3c7b4
Stephen Jones <me@stephenj.co.uk>
2025-01-01 20:56:48 +0000

    Make Keen killable (#4)
    * Make fire deadly
    
    * Fix state bug that meant death was looped
    
    Keen would die and die and die and die
    Then you'd be bombarded with infinite mars maps loaded.
    
    * Create a hazard base scene and script
    
    * Icicles, spike plants and Pat Pats created
    
    * Strangling vines built
    
    * Test out new hazards in debug level
    
    * Swap hazard sprites for scenes
    
    * Godot noise removed

---
+++ scenes/Fire.cs
@@ -0,0 +1,19 @@
+using Godot;
+
+public partial class Fire : Node2D
+{
+	private SignalManager signalManager;
+	
+	public override void _Ready()
+	{
+		signalManager = GetNode<SignalManager>("/root/SignalManager");
+	}
+
+	public void OnBodyEntered(Node2D body)
+	{
+		if (body is keen)
+		{
+			signalManager.EmitSignal(nameof(SignalManager.KeenDead));
+		}
+	}
+}
