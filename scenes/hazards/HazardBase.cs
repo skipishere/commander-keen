1b31ba5a9e2e403ae6de6fd922f78009c7904dd9
Stephen Jones <me@stephenj.co.uk>
2025-01-01 20:30:05 +0000

    Create a hazard base scene and script

---
+++ scenes/hazards/HazardBase.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System;
+using System.Diagnostics;
+
+public partial class HazardBase : Area2D
+{
+	[Export]
+	public bool RandomStartFrame = true;
+
+	[Export]
+	public AnimatedSprite2D AnimatedSprite2D;
+
+	private SignalManager signalManager;
+	
+	public override void _Ready()
+	{
+		signalManager = GetNode<SignalManager>("/root/SignalManager");
+		
+		if (RandomStartFrame)
+		{
+			var max = AnimatedSprite2D.SpriteFrames.GetFrameCount("default");
+			AnimatedSprite2D.Frame = new Random().Next(0, max);
+		}
+	}
+
+	public void OnBodyEntered(Node2D body)
+	{
+		if (body is keen)
+		{
+			Debug.WriteLine($"Keen hit a hazard - {this.Name}");
+			signalManager.EmitSignal(nameof(SignalManager.KeenDead));
+		}
+	}
+}
