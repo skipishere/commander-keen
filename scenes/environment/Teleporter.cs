631c5e062dd0b8cf0150b17d7ac621594229ee0e
Stephen Jones <me@stephenj.co.uk>
2025-01-14 09:45:18 +0000

    Try to stop excessive teleporting

--- scenes/environment/Teleporter.cs
+++ scenes/environment/Teleporter.cs
@@ -25,7 +25,7 @@ public partial class Teleporter : Area2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (inRange && Input.IsActionJustPressed("move_jump"))
+		if (inRange && Input.IsActionJustPressed("move_jump") && !isTeleporting)
 		{
 			isTeleporting = true;
 			Animate();
