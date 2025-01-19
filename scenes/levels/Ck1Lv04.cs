a573e68185ed9bf094b86f0680f8b8944569f9ca
Stephen Jones <me@stephenj.co.uk>
2025-01-19 14:57:19 +0000

    Add music and music tweaks to level 4

---
+++ scenes/levels/Ck1Lv04.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+using System.Diagnostics;
+
+public partial class Ck1Lv04 : Node2D
+{
+	[Export]
+	public AnimationPlayer Player;
+
+	[Export]
+	public Area2D Area;
+
+	private int triggerCount = 0;
+
+	private void BodyShapeEntered(Rid bodyRid, Node2D body, int bodyShapeIndex, int areaShapeIndex)
+	{
+		if (body is Keen)
+		{
+			Area.GetChild(areaShapeIndex).QueueFree();
+			
+			triggerCount++;	
+			Player.Play($"Trigger{triggerCount}");
+		}
+	}
+}
