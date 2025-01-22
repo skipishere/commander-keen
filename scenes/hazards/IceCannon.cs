6bb7dd6186dfd8cd3ed2239ff4b3fbda262b7a6e
Stephen Jones <me@stephenj.co.uk>
2025-01-22 23:27:59 +0000

    Have ice cannon spawn chunks
    Initial implementation, should also allow a left firing cannon type to be used (only 1 of them is in the game)

--- scenes/hazards/IceCannon.cs
+++ scenes/hazards/IceCannon.cs
@@ -1,19 +1,34 @@
 using Godot;
 using System;
+using System.Diagnostics;
 
 public partial class IceCannon : Node2D
 {
 	[Export]
+	private PackedScene iceChunk;
+
+	[Export]
 	public bool AimLeft = false;
+
+	private int movement;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-
+		this.movement = AimLeft ? -1 : 1;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	public void FireIceChunk()
+	{
+		var iceChunkInstance = iceChunk.Instantiate() as IceChunk;
+		iceChunkInstance.SetDirection(this.GlobalPosition, new Vector2(movement, -1), new Vector2(movement * 22, -22), this);
+		GetTree().Root.AddChild(iceChunkInstance);
+
+		Debug.WriteLine($"Firing ice chunk to the {(AimLeft ? "left" : "right")}");
 	}
 }
