a8e4c5055c0c4be0f43d7cebc584578386c88798
Stephen Jones <me@stephenj.co.uk>
2025-02-05 23:46:03 +0000

    Make chain shootable
    And cause the block to fall (& not get stuck on walls)

--- scenes/environment/StoneBlock.cs
+++ scenes/environment/StoneBlock.cs
@@ -1,24 +1,33 @@
 using Godot;
 using System;
+using System.Diagnostics;
 
 public partial class StoneBlock : CharacterBody2D, ITakeDamage
 {
 	private bool isShot = false;
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
+	private CharacterBody2D block;
+	private TileMapLayer tileMapLayer;
+	private AnimationPlayer animation;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		block = GetNode<CharacterBody2D>("StoneBlock");
+		tileMapLayer = GetNode<TileMapLayer>("StoneBlock/TileMapLayer");
+		animation = GetNode<AnimationPlayer>("AnimationPlayer");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (isShot && !IsOnFloor())
+		Debug.WriteLine($"StoneBlock on floor {block.IsOnFloor()}");
+		if (isShot && !block.IsOnFloor())
 		{
-			Velocity = Velocity with { Y = gravity/16 * (float)delta };
+			block.Velocity = block.Velocity with { Y = gravity/16 * (float)delta };
 			
-			var result = MoveAndCollide(Velocity);
+			var result = block.MoveAndCollide(block.Velocity);
 			if (result?.GetCollider() is vorticon_guard vorticon)
 			{
 				vorticon.Kill();
@@ -29,6 +38,8 @@ public partial class StoneBlock : CharacterBody2D, ITakeDamage
     public void TakeDamage()
     {
         isShot = true;
+		tileMapLayer.EraseCell(new Vector2I(-1, -3));
+		animation.Play("fall");
     }
 
 }
