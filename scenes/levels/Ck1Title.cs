365a765ce40992c3e61f032be7b548685fb6b576
Stephen Jones <me@stephenj.co.uk>
2025-02-13 23:49:43 +0000

    Wire up Continue button
    Currently duplicates the loading code

--- scenes/levels/Ck1Title.cs
+++ scenes/levels/Ck1Title.cs
@@ -20,6 +20,22 @@ public partial class Ck1Title : Node2D
 		GetTree().ChangeSceneToFile("res://main.tscn");
 	}
 
+	public void Continue()
+	{
+		var file = FileAccess.Open("user://savegame.save", FileAccess.ModeFlags.Read);
+		var savedata = file.GetAsText();
+		var json = new Json();
+		var parseResult = json.Parse(savedata);
+		if (parseResult != Error.Ok)
+		{
+			return;
+		}
+        var nodeData = new Godot.Collections.Dictionary<string, Variant>((Godot.Collections.Dictionary)json.Data);
+        game_stats.Load(nodeData);
+
+		GetTree().ChangeSceneToFile("res://main.tscn");
+	}
+
 	public void QuitGame()
 	{
 		GetTree().Quit();
