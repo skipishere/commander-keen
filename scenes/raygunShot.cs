9835147d8d763aac6c9db0dd6b95ec0b10b93b26
Stephen Jones <me@stephenj.co.uk>
2025-03-02 23:41:20 +0000

    Use GPU for Ice particles and fix raygun (#38)
    * Use particles for ice chunk
    
    Use the GPU particle effect instead of animation player
    
    * Fixes bug where shots move slow when on ice
    
    Thought bug was due to too many particles on screen but was a math error

--- scenes/raygunShot.cs
+++ scenes/raygunShot.cs
@@ -14,7 +14,6 @@ public partial class raygunShot : StaticBody2D
 	
 	public override void _Ready()
 	{
-		//direction = new Vector2(1, 0);//.Rotated(Rotation);
 		fireParticles = GetNode<GpuParticles2D>("Fire");
 		zap = GetNode<Sprite2D>("Zap");
 		collisionShape = GetNode<CollisionShape2D>("CollisionShape2D");
@@ -23,10 +22,6 @@ public partial class raygunShot : StaticBody2D
 	public void SetDirection(Vector2 globalPosition, Vector2 direction, Vector2 offset, GodotObject origin)
 	{
 		this.GlobalPosition = globalPosition + direction + offset;
-		// {
-		// 	X = globalPosition.X + (direction.X);// * shotOffset),
-		// };
-
 		this.direction = direction;
 		this.Transform  = this.Transform with { X = this.Transform.X * (direction.X > 0 ? 1 : -1) };
 		this.origin = origin;
@@ -41,7 +36,6 @@ public partial class raygunShot : StaticBody2D
 		{	
 			zap.FlipH = direction.X < 0;
 			collisionShape.Disabled = true;
-			//direction = shot.GetTravel() with { X = (float)(shot.GetTravel().X * 0.25)};
 			
 			if (shot.GetCollider() is ITakeDamage creature)
 			{
