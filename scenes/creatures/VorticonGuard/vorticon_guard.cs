950636b5e10d149cc00955ead10e34a6beaddad2
Stephen Jones <me@stephenj.co.uk>
2025-03-26 23:24:32 +0000

    Use state machine

--- scenes/creatures/VorticonGuard/vorticon_guard.cs
+++ scenes/creatures/VorticonGuard/vorticon_guard.cs
@@ -2,31 +2,27 @@ using Godot;
 using System;
 using System.Diagnostics;
 
+namespace CommanderKeen.Scenes.Creatures.Vorticon;
 public partial class vorticon_guard : CharacterBody2D, ITakeDamage
 {
 	[Export]
-	private int Health = 3;
-
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	internal int Health = 3;
 	public const float Speed = 180.0f;
 	public const float JumpVelocity = -315.0f;
 	private bool isActivated = false;
-
-	private AnimationPlayer animation;
 	private Sprite2D sprite;
-	private CollisionShape2D collisionShape;
-
 	private Timer hitTimer;
 	private SignalManager signalManager;
+	private VorticonStateMachine stateMachine = new VorticonStateMachine();
+	public float lastMovementX = Vector2.Left.X;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		animation = GetNode<AnimationPlayer>("AnimationPlayer");
 		sprite = GetNode<Sprite2D>("Sprite2D");
-		collisionShape = GetNode<CollisionShape2D>("CollisionShape2D");
 		hitTimer = GetNode<Timer>("HitTimer");
 		signalManager = GetNode<SignalManager>("/root/SignalManager");
+		stateMachine = GetNode<VorticonStateMachine>("StateMachine");
 	}
 
     private void HitTimeout()
@@ -34,52 +30,36 @@ public partial class vorticon_guard : CharacterBody2D, ITakeDamage
         (sprite.Material as ShaderMaterial).SetShaderParameter("line_thickness", 0);
     }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _PhysicsProcess(double delta)
+	{
+		if (this.Velocity.Normalized().X != 0)
+		{
+			lastMovementX = this.Velocity.Normalized().X;
+		}
+		
+		stateMachine.PhysicsProcess(delta, lastMovementX, isActivated);
+		MoveAndSlide();
+	}
+	
     public override void _Process(double delta)
 	{
-		if (!isActivated)
-		{
-			return;
-		}
+		// var velocity = Velocity;
 
-		var velocity = Velocity;
-
-		if (Health <= 0)
-		{
-			velocity.Y += gravity * (float)delta;
-			Velocity = velocity;
-			MoveAndSlide();
-			return;
-		}
-
-
-		if (!IsOnFloor())
-		{
-			velocity.Y += gravity * (float)delta;
-		}
-		else
-		{
-			velocity.Y = JumpVelocity;
-		}
-
-		Velocity = velocity;
-		if (MoveAndSlide())
-		{
-			HandleCollision();
-		}
+		// if (!IsOnFloor())
+		// {
+		// 	velocity.Y += gravity * (float)delta;
+		// }
+		// else
+		// {
+		// 	velocity.Y = JumpVelocity;
+		// }
 	}
 
-	private void HandleCollision()
+	public void BodyEntered(Node2D body)
 	{
-		for (int i = 0; i < GetSlideCollisionCount(); i++)
+		if (body is Keen)
 		{
-			var collision = GetSlideCollision(i);
-			var collider = collision.GetCollider();
-						
-			if (collider is Keen)
-			{
-				signalManager.EmitSignal(nameof(SignalManager.KeenDead));
-			}
+			signalManager.EmitSignal(nameof(SignalManager.KeenDead));
 		}
 	}
 
@@ -87,23 +67,15 @@ public partial class vorticon_guard : CharacterBody2D, ITakeDamage
 	{
 		(sprite.Material as ShaderMaterial).SetShaderParameter("line_thickness", 1);
 		hitTimer.Start();
-
-		Health--;
+		stateMachine.TakeDamage();
+		
 		Debug.WriteLine("Vorticon Guard took damage! Health: " + Health);
-		if (Health <= 0)
-		{
-			Kill();
-		}
 	}
 
 	public void Kill()
 	{
-		Health = 0;
-		Debug.WriteLine("Vorticon Guard defeated!");
-		animation.Play("die");
-		
+		stateMachine.Kill();
 	}
-
 
 	public void _on_screen_entered()
 	{
