e66a91065d1ab519af7d4fe79716e47a46c375c4
Stephen Jones <me@stephenj.co.uk>
2025-07-11 15:04:31 +0100

    Work out animation chaining

---
+++ scenes/ending/Ck1End.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+using System.Diagnostics;
+
+public partial class Ck1End : Node2D
+{
+    [Export]
+    private AnimationPlayer actionPlayer;
+
+    public override void _Ready()
+    {
+        actionPlayer.Queue("Keen rushes back");
+        actionPlayer.Queue("Shh honey");
+        actionPlayer.Queue("Billy are you");
+    }
+
+    public override void _Process(double delta)
+    {
+        //Debug.WriteLine(string.Join(" ,",actionPlayer.GetQueue()));
+        // This scene is just a cutscene, so we don't need to do anything here.
+    }
+}
