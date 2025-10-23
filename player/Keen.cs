bcee98c454fa7c74b4adc1149ac77d5d6ec07e7d
Stephen Jones <me@stephenj.co.uk>
2025-10-23 21:51:04 +0100

    More extreme value testing
    Shorten vibrate period due to the way these could chain together

--- player/Keen.cs
+++ player/Keen.cs
@@ -70,7 +70,7 @@ public partial class Keen : CharacterBody2D, ITakeDamage
         this.GlobalPosition = this.GlobalPosition with { X = xLimit, Y = yLimit };
         if (this.IsOnCeiling())
         {
-            Input.StartJoyVibration(0, 1f, 0f, 0.5f);
+            Input.StartJoyVibration(0, 0f, 1f, 0.25f);
         }
     }
 
