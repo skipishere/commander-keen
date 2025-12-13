98411d3c9a7be13740828c7e2b16867df632c0ba
Stephen Jones <me@stephenj.co.uk>
2025-12-13 22:14:56 +0000

    Make Bots immune to being pushed by Keen
    Small flaw exists, Keen is able to walk through bots which he shouldn't be able to do

--- scenes/creatures/ButlerRobot/ButlerRobot.cs
+++ scenes/creatures/ButlerRobot/ButlerRobot.cs
@@ -1,18 +1,18 @@
+using System.Diagnostics;
 using Godot;
 
-public partial class ButlerRobot : CharacterBody2D, ITakeDamage
+public partial class ButlerRobot : StaticBody2D, ITakeDamage
 {
-    public const float Speed = 0.842f;
-
-    public const float ShovePower = 2.0f;
-
+    private const float Speed = 0.842f;
+    private const int KeenCollisionLayer = 2;
     private int direction = 1;
     private AnimatedSprite2D animatedSprite2D;
     private RayCast2D rayCastLeft;
     private RayCast2D rayCastRight;
-    public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
-
+    private float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
     private Timer hitTimer;
+    private bool isPassingThrough;
+    private float passThroughStartX;
 
     public override void _Ready()
     {
@@ -24,33 +24,87 @@ public partial class ButlerRobot : CharacterBody2D, ITakeDamage
 
     public override void _PhysicsProcess(double delta)
     {
-        if (animatedSprite2D.IsPlaying() && animatedSprite2D.Animation == "turn")
-        {
-            return;
-        }
-
         if (!rayCastRight.IsColliding() && !rayCastLeft.IsColliding())
         {
             direction *= -1;
             animatedSprite2D.Play("turn");
+            EndPassThrough();
         }
-        else if (!animatedSprite2D.IsPlaying())
+        else if (!animatedSprite2D.IsPlaying() || animatedSprite2D.Animation == "turn")
         {
             animatedSprite2D.Play(direction > 0 ? "right" : "left");
         }
 
-        var velocity = new Vector2(direction * Speed, 0);
+        if (isPassingThrough)
+        {
+            var distanceMoved = Mathf.Abs(GlobalPosition.X - passThroughStartX);
+            if (distanceMoved >= 12f)
+            {
+                EndPassThrough();
+            }
+        }
 
-        var result = MoveAndCollide(velocity);
-        if (result?.GetCollider() is Keen player)
+        var velocity = new Vector2(direction * Speed, 0);
+        
+        var testParams = new PhysicsTestMotionParameters2D
         {
-            player.Shove(direction);
-        }
-        else if (result?.GetCollider() is TileMapLayer wall)
+            From = this.GlobalTransform,
+            Motion = velocity
+        };
+        var testResult = new PhysicsTestMotionResult2D();
+        
+        if (PhysicsServer2D.BodyTestMotion(this.GetRid(), testParams, testResult))
         {
-            direction *= -1;
-            animatedSprite2D.Play("turn");
+            if (testResult.GetCollider() != null)
+            {
+                var colliderObject = InstanceFromId(testResult.GetColliderId());
+                
+                if (colliderObject is Keen player)
+                {
+                    HandleKeenCollision(player, direction);
+                }
+                else if (colliderObject is TileMapLayer)
+                {
+                    HandleWallCollision();
+                }
+            }
         }
+
+        this.GlobalPosition = new Vector2(
+            this.GlobalPosition.X + velocity.X,
+            this.GlobalPosition.Y);
+    }
+
+    private void HandleKeenCollision(Keen keen, int direction)
+    {
+        if (isPassingThrough)
+        {
+            return;
+        }
+
+        var testParams = new PhysicsTestMotionParameters2D
+        {
+            From = keen.GlobalTransform,
+            Motion = new Vector2(direction * 3f, 0)
+        };
+        var testResult = new PhysicsTestMotionResult2D();
+        var canPush = !PhysicsServer2D.BodyTestMotion(keen.GetRid(), testParams, testResult);
+
+        if (canPush)
+        {
+            keen.Shove(direction);
+        }
+        else
+        {
+            StartPassThrough();
+        }
+    }
+
+    private void HandleWallCollision()
+    {
+        direction *= -1;
+        animatedSprite2D.Play("turn");
+        EndPassThrough();
     }
 
     public void TakeDamage()
@@ -62,5 +116,18 @@ public partial class ButlerRobot : CharacterBody2D, ITakeDamage
     private void HitTimeout()
     {
         (animatedSprite2D.Material as ShaderMaterial).SetShaderParameter("line_thickness", 0);
+    }
+
+    private void StartPassThrough()
+    {
+        SetCollisionMaskValue(KeenCollisionLayer, false);
+        isPassingThrough = true;
+        passThroughStartX = GlobalPosition.X;
+    }
+
+    private void EndPassThrough()
+    {
+        SetCollisionMaskValue(KeenCollisionLayer, true);
+        isPassingThrough = false;
     }
 }
