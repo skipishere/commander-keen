55686543fcbbe6bbb5eb99bb78a15211b03501e5
Stephen Jones <me@stephenj.co.uk>
2026-03-11 00:56:15 +0000

    Rethink Tank Robot shoot state and add raygun shot

--- scenes/creatures/TankRobot/ShootState.cs
+++ scenes/creatures/TankRobot/ShootState.cs
@@ -1,18 +1,21 @@
-using System;
 using Godot;
 
 namespace CommanderKeen.Scenes.Creatures.TankRobot;
 
 public partial class ShootState : TankRobotBaseState
 {
+    [Export]
+    private PackedScene raygun;
+
+    [Export]
+    private Color raygunColor;
+
     public override TankRobotStateMachine.TankRobotStates StateType => TankRobotStateMachine.TankRobotStates.Shoot;
+    private Timer waitTimer;
 
     public override void _Ready()
     {
-    }
-
-    public override void StateInput(InputEvent inputEvent)
-    {
+        waitTimer = GetNode<Timer>("WaitTimer");
     }
 
     public override void PhysicsProcess(double delta, float lastMovementX)
@@ -22,11 +25,22 @@ public partial class ShootState : TankRobotBaseState
     public void ShootFinished()
     {
         NextState = TankRobotStateMachine.TankRobotStates.Thinking;
+        AnimationTree.Active = true;
     }
 
     public override void Enter()
     {
-        playback.Travel("Shoot");
-        AnimationTree.Set("parameters/Shoot/blend_position", Character.Direction);
+        waitTimer.Start();
+        AnimationTree.Active = false;
+
+        var shotPosition = Character.GlobalPosition + new Vector2(Character.Direction * 10, -2);
+
+        var raygunInstance = raygun.Instantiate() as raygunShot;
+        raygunInstance.ShotColor = raygunColor;
+        raygunInstance.SetDirection(
+            shotPosition,
+            new Vector2(Character.Direction, 0),
+            Character);
+        GetTree().Root.AddChild(raygunInstance);
     }
 }
